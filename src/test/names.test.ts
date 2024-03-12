597ac051e4b48ca4c97b5fc48fdf09678ac6d013
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2024-03-12 10:26:47 +0100

    Even more formatting of date.

--- src/test/names.test.ts
+++ src/test/names.test.ts
@@ -4,8 +4,10 @@ import namesSorted from "../data/namesAlphabetic";
 let names = namesSorted;
 
 test("dates are formatted properly", () => {
-  names.forEach(({ date }) => {
+  names.forEach(({ date, dateISO }) => {
     expect(new Date(date).getDate()).not.toBeNaN();
     expect(new Date(date).getMonth()).not.toBeNaN();
+    expect(new Date(dateISO).getDate()).not.toBeNaN();
+    expect(new Date(dateISO).getMonth()).not.toBeNaN();
   });
 });
